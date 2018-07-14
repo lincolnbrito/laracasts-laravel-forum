590177b336dedc905090ed7eb5b0b8cceedcdb4a
Lincoln Brito <lincoln.sbrito@gmail.com>
2018-07-14 18:58:35 -0300

    test if a user can read a single thread

--- forum/tests/Feature/ThreadsTest.php
+++ forum/tests/Feature/ThreadsTest.php
@@ -10,12 +10,19 @@ class ThreadsTest extends TestCase
     use DatabaseMigrations;
     
     /** @test */
-    public function a_user_can_browse_threads()
+    public function a_user_can_view_all_threads()
     {
         $thread = factory('App\Thread')->create();
         
         $response = $this->get('/threads');
         $response->assertSee($thread->title);
+       
+    }
+
+    /** @test */
+    public function a_user_can_read_a_single_threads()
+    {
+        $thread = factory('App\Thread')->create();       
 
         $response = $this->get('/threads/' . $thread->id);
         $response->assertSee($thread->title);
