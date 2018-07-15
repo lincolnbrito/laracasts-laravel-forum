8d3fc46f03cdc411a034d1d8f6739ad7cea5947c
Lincoln Brito <lincoln.sbrito@gmail.com>
2018-07-14 23:30:09 -0300

    test if an authenticated user can create threads

--- forum/tests/Feature/CreateThreadsTest.php
+++ forum/tests/Feature/CreateThreadsTest.php
@@ -5,16 +5,38 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
+
 
 class CreateThreadsTest extends TestCase
 {
+    use DatabaseMigrations;
+
+    /** @test */
+    function guest_may_not_create_threads()
+    {
+
+        $this->expectException('Illuminate\Auth\AuthenticationException');
+
+        $thread = factory('App\Thread')->make();
+        
+        $this->post('/threads', $thread->toArray());   
+    }
+
     /** @test */
     function an_authenticated_user_can_create_forum_threads()
     {
-        // Giver we have a signed user
+        // Given we have a signed user
+        $this->actingAs(factory('App\User')->create());
+        
         // Whe we hit the endpoint to create a new thread
+        $thread = factory('App\Thread')->make();
+        $this->post('/threads', $thread->toArray());
+
         // Then, when we visit the thread page.
         // We should see the new thread
+        $this->get($thread->path())
+             ->assertSee($thread->title)
+             ->assertSee($thread->body);
+
     }
 }
