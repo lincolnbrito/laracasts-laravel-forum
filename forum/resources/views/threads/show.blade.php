9c660aab28dae08de0a7e45c256fb08ddc372771
Lincoln Brito <lincoln.sbrito@gmail.com>
2018-07-14 21:28:33 -0300

    test if a user can see replies

--- forum/resources/views/threads/show.blade.php
+++ forum/resources/views/threads/show.blade.php
@@ -20,16 +20,18 @@
     </div>
     <div class="row">
         <div class="col-md-8 col-md-offset-2">            
+            @foreach($thread->replies as $reply)            
             <div class="panel panel-default">
-                <div class="panel-heading">Replies</div>
-                <div class="panel-body">
-                @foreach($thread->replies as $reply)
-                    {{ $reply->body }}
-                    <hr>
-                @endforeach
-                
+                <div class="panel-heading">
+                    <a href="#">
+                        {{ $reply->owner->name }}
+                    </a> said {{ $reply->created_at->diffForHumans() }}
                 </div>
-            </div>            
+                <div class="panel-body">
+                    {{ $reply->body }}                            
+                </div>
+            </div>
+            @endforeach
         </div>
     </div>
 </div>
