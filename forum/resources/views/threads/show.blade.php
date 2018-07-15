12cfa33facb50c33016a17b7f9b254bc919410a8
Lincoln Brito <lincoln.sbrito@gmail.com>
2018-07-14 21:14:06 -0300

    show replies on panel

--- forum/resources/views/threads/show.blade.php
+++ forum/resources/views/threads/show.blade.php
@@ -19,14 +19,17 @@
         </div>
     </div>
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            @foreach($thread->replies as $reply)
+        <div class="col-md-8 col-md-offset-2">            
             <div class="panel panel-default">
-                <div class="panel-body">                   
-                    {{ $reply->body }}                   
+                <div class="panel-heading">Replies</div>
+                <div class="panel-body">
+                @foreach($thread->replies as $reply)
+                    {{ $reply->body }}
+                    <hr>
+                @endforeach
+                
                 </div>
-            </div>
-            @endforeach
+            </div>            
         </div>
     </div>
 </div>
