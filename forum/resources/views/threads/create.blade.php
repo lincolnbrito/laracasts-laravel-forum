4f0bcc3b5838e320829fc84686a9014253c2127c
Lincoln Brito <lincoln.sbrito@gmail.com>
2018-07-15 20:06:06 -0300

    finish create form

--- forum/resources/views/threads/create.blade.php
+++ forum/resources/views/threads/create.blade.php
@@ -8,7 +8,20 @@
                 <div class="panel-heading">Create a New Thread</div>
 
                 <div class="panel-body">
-                   
+                    <form method="POST" action="/threads">
+                        {{ csrf_field() }}
+                        <div class="form-group">
+                            <label for="title">Title</label>
+                            <input type="text" name="title" id="title" class="form-control" placeholder="Title">
+                        </div>
+
+                        <div class="form-group">
+                            <label for="body">Body:</label>
+                            <textarea name="body" id="body" class="form-control" placeholder="Body" rows="8"></textarea>
+                        </div>
+
+                        <button type="submit" class="btn btn-primary">Publish</button>
+                    </form>
                 </div>
             </div>
         </div>
