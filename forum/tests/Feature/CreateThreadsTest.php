af9b3d75c49ed5c969780ee1af19ab56195da1a8
Lincoln Brito <lincoln.sbrito@gmail.com>
2018-07-15 23:32:41 -0300

    test validation errors

--- forum/tests/Feature/CreateThreadsTest.php
+++ forum/tests/Feature/CreateThreadsTest.php
@@ -28,17 +28,55 @@ class CreateThreadsTest extends TestCase
     function an_authenticated_user_can_create_forum_threads()
     {
         // Given we have a signed user
-        $this->signIn();
-        
         // Whe we hit the endpoint to create a new thread
-        $thread = create('App\Thread');
-        $this->post('/threads', $thread->toArray());
-
         // Then, when we visit the thread page.
         // We should see the new thread
-        $this->get($thread->path())
+
+        $this->signIn();
+
+        $thread = create('App\Thread');
+        $response = $this->post('/threads', $thread->toArray());
+
+        $this->get($response->headers->get('Location'))
              ->assertSee($thread->title)
              ->assertSee($thread->body);
+
+    }
+
+    /** @test */
+    function a_thread_requires_a_title()
+    {
+        $this->publishThread(['title'=>null])
+            ->assertSessionHasErrors('title');
+    }
+
+    /** @test */
+    function a_thread_requires_a_body()
+    {
+        $this->publishThread(['body'=>null])
+            ->assertSessionHasErrors('body');
+    }
+
+    /** @test */
+    function a_thread_requires_a_valid_channel()
+    {
+        factory('App\Channel',2)->create();
+
+        $this->publishThread(['channel_id'=>null])
+            ->assertSessionHasErrors('channel_id');
+
+        $this->publishThread(['channel_id'=>999])
+            ->assertSessionHasErrors('channel_id');
+    }
+
+    public function publishThread($overrides = [])
+    {
+        $this->withExceptionHandling()
+            ->signIn();
+
+        $thread = make('App\Thread', $overrides);
+
+        return $this->post('/threads', $thread->toArray());
 
     }
 }
