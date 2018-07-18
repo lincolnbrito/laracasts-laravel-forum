2d36150a21aded2576ef9eda16caf516011e8fd9
Lincoln Brito <lincoln.sbrito@gmail.com>
2018-07-18 10:39:17 -0300

    Filter threads by popularity
    - add 'popular' filter option to ThreadFilters
    - add menu item to browse menu
    - return json if request wants json (helps in test)

--- forum/app/Filters/ThreadFilters.php
+++ forum/app/Filters/ThreadFilters.php
@@ -7,7 +7,7 @@ use App\User;
 class ThreadFilters extends Filters
 {
 
-    protected $filters = ['by'];
+    protected $filters = ['by', 'popular'];
 
     /**
      * Filter the query by a given username
@@ -19,5 +19,18 @@ class ThreadFilters extends Filters
         $user = User::where('name', $username)->firstOrFail();
 
         return $this->builder->where('user_id', $user->id);
+    }
+
+    /**
+     * Filter the query according the most popular threads
+     *
+     * @return mixed
+     */
+    protected function popular()
+    {
+        //remove default orders when popular is set
+        $this->builder->getQuery()->orders = [];
+
+        return $this->builder->orderBy('replies_count', 'desc');
     }
 }
--- forum/resources/views/layouts/app.blade.php
+++ forum/resources/views/layouts/app.blade.php
@@ -49,6 +49,7 @@
                                 @if(auth()->check())
                                 <li><a href="/threads?by={{ auth()->user()->name }}">My Threads</a></li>
                                 @endif
+                                <li><a href="/threads?popular=1">Popular Threads</a></li>
                             </ul>
                         </li>
 
