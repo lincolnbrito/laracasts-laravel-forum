c652981aeeb81a4f0068e7462957c2daf785dc63
Lincoln Brito <lincoln.sbrito@gmail.com>
2018-07-19 15:26:52 -0300

    show threads created by user on their profile

--- forum/resources/views/profiles/show.blade.php
+++ forum/resources/views/profiles/show.blade.php
@@ -8,5 +8,23 @@
                 <small>Since {{ $profileUser->created_at->diffForHumans() }}</small>
             </h1>
         </div>
+
+        @foreach($profileUser->threads as $thread)
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <a href="#">{{ $thread->creator->name }}</a> posted:
+                    {{ $thread->title }}
+                </div>
+
+                <div class="panel-body">
+                    <article>
+                        <h4>{{ $thread->title }}</h4>
+                        <div class="body">
+                            {{ $thread->body }}
+                        </div>
+                    </article>
+                </div>
+            </div>
+        @endforeach
     </div>
 @endsection
