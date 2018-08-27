f0ace92a5552f1639a578425f690a8a69f2b8d8f
Lincoln Brito <lincoln.sbrito@gmail.com>
2018-08-27 15:44:49 -0300

    #27 - Creating controller queries to the model

--- forum/app/Http/Controllers/ProfilesController.php
+++ forum/app/Http/Controllers/ProfilesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Activity;
 use App\User;
 use Illuminate\Http\Request;
 
@@ -11,18 +12,7 @@ class ProfilesController extends Controller
     {
         return view('profiles.show', [
             'profileUser' => $user,
-            'activities' => \App\Activity::feed($user)
+            'activities' => Activity::feed($user)
         ]);
-    }
-
-    /**
-     * @param User $user
-     * @return \Illuminate\Database\Eloquent\Collection|\Illuminate\Database\Eloquent\Relations\HasMany[]|\Illuminate\Database\Query\Builder[]|\Illuminate\Support\Collection
-     */
-    protected function getActivity(User $user)
-    {
-        return $user->activity()->take(50)->latest()->with('subject')->get()->groupBy(function ($activity) {
-            return $activity->created_at->format('Y-m-d');
-        });
     }
 }
