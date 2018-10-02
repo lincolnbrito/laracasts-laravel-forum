f8d58ebdeeb6a08d2bd21cab899107e4f6144b7e
Lincoln Brito <lincoln.sbrito@gmail.com>
2018-10-02 13:30:43 -0300

    flash message

---
+++ forum/resources/assets/js/components/Flash.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="alert alert-warning" role="alert">
+        <strong>Success!</strong> {{ body }}
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                body: 'Temp for now'
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
